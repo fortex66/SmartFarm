dfd4ac1b581d5a840ada1fafef38c8ef06ba598e
fortex66 <71388651+fortex66@users.noreply.github.com>
2024-01-04 20:14:53 +0900

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -5,8 +5,8 @@
     // 데이터 베이스 연결 설정
     $host = "localhost";
     $username = "root"; // 데이터베이스 사용자 이름
-    $password = "sang1130"; // 데이터베이스 비밀번호
-    $dbname = "mydb"; // 데이터베이스 이름
+    $password = "*******"; // 데이터베이스 비밀번호
+    $dbname = "*****"; // 데이터베이스 이름
 
     // 데이터베이스 연결
     $con = new mysqli($host, $username, $password, $dbname);
